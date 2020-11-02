cecd1a71dcadaa3a2d9e0c84c1f3302f787d84b4
Ramdoni007 <ramd42156@gmail.com>
2020-11-02 10:54:41 +0700

    Penambahan Materi DOM JS bagian 2

--- app.js
+++ app.js
@@ -14,4 +14,30 @@ init = document.doctype;
 init = document.domain;
 init = document.URL;
 
+init = document.characterSet;
+init = document.contentType;
+init = document.forms;
+init = document.forms[0];
+init = document.forms[0].id;
+init = document.forms[0].method;
+init = document.forms[0].action;
+
+// init = document.links;
+// init = document.links[0].className;
+// init = document.links[0].id;
+// init = document.links[0].classList;
+
+// init = document.images;
+init = document.scripts;
+init = document.scripts[1];
+init = document.scripts[1].getAttribute("src");
+
+let scripts = document.scripts;
+
+let scriptsArr = Array.from(scripts);
+
+scriptsArr.forEach(function (scripts) {
+  console.log(scripts.getAttribute("src"));
+});
+
 console.log(init);
