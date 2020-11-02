df9ca917b17f3524ae4b83b34ad002a2288db4df
Ramdoni007 <ramd42156@gmail.com>
2020-11-02 10:05:40 +0700

    Penambahan Materi di dalam App.js

---
+++ app.js
@@ -0,0 +1,17 @@
+let init;
+
+// Di dalam DOM kita bisa melakukan Melihat,akses,dan juga merubah
+// file html yang kita miliki .
+//contohnya seperti di bawah ini
+
+init = document;
+init = document.all;
+init = document.all[2];
+init = document.all.length;
+init = document.head;
+init = document.body;
+init = document.doctype;
+init = document.domain;
+init = document.URL;
+
+console.log(init);
